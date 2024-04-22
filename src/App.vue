b789d8b17fb7fb6a6b0a4034748483391cbee798
erickhenrique3 <erick.2henriquehtp@gmail.com>
2024-04-22 10:58:46 -0300

    Login começado

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  <Capa/>
+  <!-- <Capa/> -->
+  <Login/>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Capa from './components/Capa.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-   Capa
+   Capa,
+   Login
   }
 }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="container">
+        <div class="box1">
+
+        </div>
+        <div class="box2">
+            <form>
+                <h1>Login</h1>
+                <label for="form-label">Endereço de e-mail</label>
+                <input type="text" name="email" id="" placeholder="Digite seu e-mail" autocomplete="off">
+
+                <label for="form-label">Senha de acesso:</label>
+                <input type="password" name="password" id="" placeholder="Digite sua senha" autocomplete="off">
+
+                <a href="#">Não tenho conta</a>
+                <button>Acessar</button>
+            </form>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+}
+</script>
+
+<style scoped>
+.container {
+    width: 100vw;
+    height: 100vh;
+
+    /* display: grid; */
+    /* grid-template-columns: 1fr 1fr; */
+    position: relative;
+    background-color: #f5f5f5;
+    box-sizing: border-box;
+    /* border: 10px solid; */
+
+}
+
+.box1 {
+    position: absolute;
+
+    background-color: #db3535;
+    width: 50%;
+    height: 100vh;
+
+
+
+}
+
+.box2 {
+
+    position: absolute;
+    width: 50%;
+    height: 100%;
+    left: 50%;
+    background-color: rgb(198, 188, 188);
+
+}
+
+form {
+    position: relative;
+    width: 50%;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+}
+
+form > input {
+    border: none;
+    width: 100%;
+    height: 50px;
+}
+
+form > label {
+    padding: 10px 0px;
+}
+
+form button {
+
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+    width: 150px;
+    height: 50px;
+    background-color: #000000;
+    color: #ffffff;
+    border: none;
+    
+    cursor: pointer;
+}
+
+form>a {
+    position: absolute;
+    bottom: 10px;
+    left: 0px;
+    text-decoration: none;
+    color: gray;
+}
+
+form>a:hover {
+    text-decoration: underline;
+
+}
+</style>
