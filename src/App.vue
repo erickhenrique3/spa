35a9555756c3c716553c8338b441c7cb80ee0507
erickhenrique3 <erick.2henriquehtp@gmail.com>
2024-04-22 11:19:31 -0300

    cadastro começado

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <!-- <Capa/> -->
-  <Login/>
+  <!-- <Login/> -->
+  <Cadastro/>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import Capa from './components/Capa.vue'
 import Login from './components/Login.vue'
-
+import Cadastro from './components/Cadastro.vue'
 export default {
   name: 'App',
   components: {
    Capa,
-   Login
+   Login,
+   Cadastro,
   }
 }
 </script>
---
+++ src/components/Cadastro.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="container">
+        <div class="box1">
+            <form>
+                <h1>Inscreva-se</h1>
+                <label for="form-label">Nome completo</label>
+                <input type="text" name="name" id="" placeholder="Digite seu nome" autocomplete="off">
+
+                <label for="form-label">Endereço de e-mail</label>
+                <input type="text" name="email" id="" placeholder="Digite seu e-mail" autocomplete="off">
+
+                <label for="form-label">Senha de acesso:</label>
+                <input type="password" name="password" id="" placeholder="Digite sua senha" autocomplete="off">
+
+                <a href="#">Já tenho uma conta</a>
+                <button>Cadastrar</button>
+            </form>
+        </div>
+        <div class="box2">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Cadastro'
+} 
+</script>
+
+<style scoped>
+.container {
+    width: 100vw;
+    height: 100vh;
+
+    /* display: grid; */
+    /* grid-template-columns: 1fr 1fr; */
+    position: relative;
+    background-color: #f5f5f5;
+    box-sizing: border-box;
+}
+
+.box1 {
+    position: absolute;
+    background-color: #fda1a1;
+    width: 50%;
+    height: 100vh;
+}
+
+.box2 {
+    position: absolute;
+    width: 50%;
+    height: 100%;
+    left: 50%;
+    background-color: rgb(237, 17, 17);
+}
+
+form {
+    position: relative;
+    width: 50%;
+    height: 450px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+form > input {
+    border: none;
+    width: 100%;
+    height: 50px;
+}
+
+form>label {
+    padding: 10px 0px;
+}
+
+form button {
+
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+    width: 150px;
+    height: 50px;
+    background-color: #000000;
+    color: #ffffff;
+    border: none;
+
+    cursor: pointer;
+}
+
+form>a {
+    position: absolute;
+    bottom: 10px;
+    left: 0px;
+    text-decoration: none;
+    color: gray;
+}
+
+form>a:hover {
+    text-decoration: underline;
+
+}
+</style>
